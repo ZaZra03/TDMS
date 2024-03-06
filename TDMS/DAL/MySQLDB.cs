abf8ed97a2f0d19f20221a8168206ebdb448b7c6
Ezra Micah Malsi <malsiezramicah3@gmail.com>
2024-03-06 08:33:48 +0800

    Added Menu Items and User Window.

---
+++ TDMS/DAL/MySQLDB.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDMS.DAL
+{
+    class MySQLDB
+    {
+        private string connectionString = "Server=localhost;Database=tdms;Uid=root;Pwd=1234;";
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable dataTable = new DataTable();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+                connection.Close();
+            }
+            return dataTable;
+        }
+
+        public void ExecuteNonQuery(string query)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+    }
+}
--- TDMS/obj/Debug/net8.0-windows/TDMS.AssemblyInfo.cs
+++ TDMS/obj/Debug/net8.0-windows/TDMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253d524d1b82244b7a576b986ff6bc0f5c7d59cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f93c5213fd29f83472d5e0568745a8aaddc5a93")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
