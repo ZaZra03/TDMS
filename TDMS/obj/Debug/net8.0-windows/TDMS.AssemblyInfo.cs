91864475be90b7faa31d69debfe7933f346dc966
Ezra Micah Malsi <malsiezramicah3@gmail.com>
2024-03-18 23:17:26 +0800

    Added the TimeVM and Time View in Data Template.

--- TDMS/obj/Debug/net8.0-windows/TDMS.AssemblyInfo.cs
+++ TDMS/obj/Debug/net8.0-windows/TDMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3bfc3865f2cebc15a170406a599bb4f91916bde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+809432a70fb7fc62e4be3fd4610e6e8ec3875e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
